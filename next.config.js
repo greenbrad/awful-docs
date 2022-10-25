5da2daca39143ab00a24791369b50cf22c7df730
Brad Green <green.brad@live.com>
2022-10-25 00:40:11 -0400

    init

---
+++ next.config.js
@@ -0,0 +1,57 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+});
+
+module.exports = withNextra({
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  redirects: () => {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 301,
+      },
+      {
+        source: "/namespaces_and_loading/namespaces",
+        destination: "/docs/namespaces_and_loading/namespaces",
+        statusCode: 301,
+      },
+      {
+        source: "/advanced/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/cache",
+        destination: "/docs/advanced/cache",
+        statusCode: 301,
+      },
+      {
+        source: "/docs/options",
+        destination: "/docs/api",
+        statusCode: 301
+      },
+      {
+        source: "/change-log",
+        destination: "/docs/change-log",
+        statusCode: 301,
+      },
+      {
+        source: "/blog/swr-1",
+        destination: "/blog/swr-v1",
+        statusCode: 301,
+      },
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        statusCode: 302,
+      },
+    ];
+  },
+});
